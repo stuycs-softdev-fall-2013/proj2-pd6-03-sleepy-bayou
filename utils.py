0752a1ad7dc5dbdd1b2091a7150a8e06d6e2d351
Jane Argodale <jja8@Jane-Argodales-MacBook-Pro.local>
2013-11-30 20:59:27 -0500

    added mongodb stuff to utils.py to allow for login and registration functionality

---
+++ utils.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from flask import session
+c = MongoClient()
+db = c.users
+
+def authorize(username, password):
+    user = db.Collections.find_one({'username':username, 'password':password})
+    if user:
+        return user["id"]
+    else:
+        return None
+def userExists(username):
+    return len(list(db.Collections.find({'username':username}))) == 1
+def createUser(username, password):
+    if not userExists(username):
+        ui = db.Collections.count()+1
+        db.Collections.insert({'id':ui,'username':username, 'password':password})
+        return True
+    else:
+        return False

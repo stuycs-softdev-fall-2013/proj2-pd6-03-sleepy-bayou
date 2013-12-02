01efa7c48838ff782875fff4ce5bf8a3e0f3c670
Jeffrey Yin <jyin96@gmail.com>
2013-12-01 21:30:36 -0500

    worked on login stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, redirect, url_for, session, request
-
+import utils
 app = Flask(__name__)
 app.secret_key = "SUBMIT!"
 
@@ -12,9 +12,8 @@ def home():
 def register():
     if request.method=="POST":
         if request.form["password"]==request.form["password2"]:
-            print("Done")
             #createUser will return a number depending on what the error was
-            result=utils.createUser(str(request.form("username")).lower(),request.form("password"))
+            result=utils.createUser(request.form["username"].lower(),request.form["password"])
             #success. Login page will have confirmation message
             if result==0:
                 return redirect("/login?type=2")
@@ -28,20 +27,22 @@ def register():
         else:
             return render_template("register.html",type=3)
     else:
-        print("argh")
         return render_template("register.html")
 @app.route("/login")
 def login():
-    if request.method == "POST":
-        result = app.authorize(str(request.form["username"]).lower(), str(request.form["password"]))
+    if request.method=="POST":
+        print("done")
+        result = utils.authorize(str(request.form["username"]).lower(), str(request.form["password"]))
         #successful login
         if result == 0:  
             session["username"] = request.form["username"]
-            returnredirect("/?type=2")
+            return redirect("/?type=2")
         #failed attempt!
         else:
             return render_template("login.html",type=1)
     else:
+        if request.args.get("type") == "2":
+            return render_template("login.html",type=2)
         return render_template("login.html")
 @app.route("/logout")
 def logout():
--- utils.py
+++ utils.py
@@ -12,9 +12,11 @@ def authorize(username, password):
 def userExists(username):
     return len(list(db.Collections.find({'username':username}))) == 1
 def createUser(username, password):
+    if len(username) < 4 or len(password) < 6:
+        return 2
     if not userExists(username):
         ui = db.Collections.count()+1
         db.Collections.insert({'id':ui,'username':username, 'password':password})
-        return True
+        return 0
     else:
-        return False
+        return 1

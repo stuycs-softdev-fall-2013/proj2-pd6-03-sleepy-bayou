7a8b829b5f2f6a0a5a2e632ff77919a79e9ff729
Jeffrey Yin <jyin96@gmail.com>
2013-12-01 19:55:13 -0500

    added new file paths

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, session, request
 app = Flask(__name__)
 app.secret_key = "SUBMIT!"
 
-@app.route("/foodplanner")
+@app.route("/")
 def home():
     return render_template("index.html")
 
@@ -36,7 +36,7 @@ def login():
         if result == 0:  
             session["username"] = request.form["username"]
             returnredirect("/?type=2")
-        #fail fail fail!
+        #failed attempt!
         else:
             return render_template("login.html",type=1)
     else:
@@ -45,7 +45,19 @@ def login():
 def logout():
     session.pop("username",None)
     return redirect("/?type=2")
-                
+
+@app.route("/route")
+def route():
+    if request.method=="POST":
+        start = request.form["start"]
+        end = request.form["end"]
+        #fields were left blank
+        if start == None or end == None:
+            return render_template("route.html", error=1)
+        results = utils.compute(start, end)
+        return redirect("/results")
+    else:
+        return render_template("route.html")
                 
 
 
